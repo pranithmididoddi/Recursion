834789a6d3ca97011ad8f7f8e3ef3255e2642af0
Srinivas <pranith13@gmail.com>
2017-11-05 17:17:29 -0600

    Overlapping subproblems

--- src/Recursion.java
+++ src/Recursion.java
@@ -2,18 +2,36 @@ public class Recursion {
 
     public static void main(String[] args){
 
-        Recursion object=new Recursion();
 
-        System.out.println(object.calculatepower(5,3));
+       Recursion recursion=new Recursion();
+        System.out.println(recursion.mincost(0,1));
 
 
     }
 
-    public int calculatepower(int m, int n){
-        if(n==0) return 1;
-        else if(m==1) return 1;
-        else return m*calculatepower(m,n-1);
+    /**calculating the minimum cost between the source and destination*/
+
+    public int mincost(int s, int d){
+
+        int[][] cost={{0,15,45,75},{-1,0,20,30},{-1,-1,0,30},{-1,-1,-1,0}};;
+
+        if(s==d || s==d-1) {
+            return cost[s][d];
+        }
+        int minvalue=cost[s][d];
+
+        for(int i=s+1;i<d;i++){
+            int temp=mincost(s,i)+mincost(i,d);
+
+            if(temp<minvalue){
+                minvalue=temp;
+            }
+        }
+
+        return minvalue;
     }
+
+
 
 
 }
